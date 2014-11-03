9127e1f090401e36f415dd069975477c7b5addc9
Mike Zukowsky <michal@durooil.com>
2014-11-03 11:18:47 +0100

    Create dompdfFactory.php

---
+++ src/dompdfmodule/Service/dompdfFactory.php
@@ -0,0 +1,13 @@
+<?php
+namespace dompdfmodule\Service;
+
+use Zend\ServiceManager\AbstractFactory;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class dompdfFactory extends AbstractFactory
+{
+    public function createService(ServiceLocatorInterface $sm)
+    {
+    
+    }
+}
