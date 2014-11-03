d459e9a4dc2ca4a99dcccfe23e32f003fbce47f1
Mike Zukowsky <michal@durooil.com>
2014-11-03 11:38:58 +0100

    Update dompdfFactory.php

--- src/dompdfmodule/Service/dompdfFactory.php
+++ src/dompdfmodule/Service/dompdfFactory.php
@@ -6,8 +6,27 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 
 class dompdfFactory extends AbstractFactory
 {
+    /** @var bool */
+    protected static $initialized = false;
+    
     public function createService(ServiceLocatorInterface $sm)
     {
-    
+        // service is not shared
+        // but don't register constants each time
+        
+        if (! self::$initialized) {
+            $config = $sm->get('config');
+            $dompdfConfig = isset($config['dompdf']) ? $config['dompdf'] : array();
+            self::$initialized = true;
+        
+            foreach ($dompdfConfig as $settingName => $settingValue) {
+                if (! defined($settingName)) {
+                    define($settingName, $settingValue);
+                }
+            }
+        }
+        
+        $dompdf = new DOMPDF();
+        return $dompdf;
     }
 }
