ac26fb68a7480739a15157f0512ca3c1c4b1169b
Mike Zukowsky <michal@durooil.com>
2014-11-03 12:13:27 +0100

    Update dompdfFactory.php

--- src/dompdfmodule/Service/dompdfFactory.php
+++ src/dompdfmodule/Service/dompdfFactory.php
@@ -16,8 +16,10 @@ class dompdfFactory implements FactoryInterface
         
         if (! self::$initialized) {
             $config = $serviceLocator->get('config');
-            $dompdfConfig = isset($config['dompdf']) ? $config['dompdf'] : array();
+            $userConfig = isset($config['dompdf']) ? $config['dompdf'] : array();
             self::$initialized = true;
+        
+            $dompdfConfig = array_merge($this->createDefaultSettings(), $userConfig);
         
             foreach ($dompdfConfig as $settingName => $settingValue) {
                 if (! defined($settingName)) {
@@ -28,5 +30,39 @@ class dompdfFactory implements FactoryInterface
         
         $dompdf = new DOMPDF();
         return $dompdf;
+    }
+    
+    /**
+     * Some settings can be evaluated by default.
+     * @return array
+     */
+    protected function createDefaultSettings()
+    {
+        return array(
+            'DOMPDF_TEMP_DIR'              => sys_get_temp_dir(),
+            'DOMPDF_FONT_DIR'              => DOMPDF_FONT_DIR . '/lib/fonts',
+            'DOMPDF_FONT_CACHE'            => DOMPDF_FONT_DIR . '/lib/fonts',
+            'DOMPDF_CHROOT'                => '',
+            'DOMPDF_LOG_OUTPUT_FILE'       => 'data/dompdf.log',
+            'DOMPDF_DEFAULT_MEDIA_TYPE'    => 'screen',
+            'DOMPDF_DEFAULT_PAPER_SIZE'    => \CPDF_Adapter::$PAPER_SIZES['a4'],
+            'DOMPDF_DEFAULT_FONT'          => 'serif',
+            'DOMPDF_DPI'                   => 96,
+            'DOMPDF_FONT_HEIGHT_RATIO'     => 1.1,
+            'DOMPDF_UNICODE_ENABLED'       => true,
+            'DOMPDF_ENABLE_PHP'            => false,
+            'DOMPDF_ENABLE_REMOTE'         => false,
+            'DOMPDF_ENABLE_CSS_FLOAT'      => true,
+            'DOMPDF_ENABLE_JAVASCRIPT'     => false,
+            'DOMPDF_ENABLE_HTML5PARSER'    => true,
+            'DOMPDF_ENABLE_FONTSUBSETTING' => false,
+            
+            'DEBUGPNG' => false,
+            'DEBUGKEEPTEMP' => false,
+                        'DEBUGPNG' => false,
+                                    'DEBUGPNG' => false,
+                                                'DEBUGPNG' => false,
+                                                            'DEBUGPNG' => false,
+        );
     }
 }
