8a799caacec65c98906128eefdd68d5b4556d587
Mike Zukowsky <michal@durooil.com>
2014-11-03 11:59:13 +0100

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,10 @@
 <?php
 
 return array(
+    'dompdf' => array(
+        // constant name => constant value
+        // from DOMPDF documentation
+    ),
     'service_manager' => array(
         'shared' => array(
             'dompdf' => false,
