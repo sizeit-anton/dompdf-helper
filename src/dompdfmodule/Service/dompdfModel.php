4a01a2daf2ed664833bdfeeefa6a13f3d17aaa6b
Mike Zukowsky <michal@durooil.com>
2014-11-03 11:26:40 +0100

    Update dompdfModel.php

--- src/dompdfmodule/Service/dompdfModel.php
+++ src/dompdfmodule/Service/dompdfModel.php
@@ -3,5 +3,24 @@ namespace dompdfmodule;
 
 class dompdfModel implements ModelInterface
 {
+    /** @var array */
+    protected $attributes;
     
+     /**
+      * @param array $attributes
+      * @return void
+      */
+    public function setAttributes(array $attributes)
+    {
+        $this->attributes = $attributes;
+    }
+    
+    /**
+     * @param array $attributes
+     * @return array
+    */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
 }
