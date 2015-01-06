634260c31c201673b004158224c3356c4320e804
Mike Zukowsky <michal@durooil.com>
2015-01-06 10:48:58 +0100

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -1,10 +1,13 @@
 <?php
 
 return array(
+    /*
+    // you can override default library settings in your configuration as follows:
     'dompdf' => array(
         // constant name => constant value
         // from DOMPDF documentation
     ),
+    */
     'service_manager' => array(
         'shared' => array(
             'dompdf' => false,
