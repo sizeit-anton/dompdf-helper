a5f7aa1803181fd74ea766e08cd68394d25a906a
Michał Żukowski <michalz@plocman.pl>
2016-08-25 15:14:58 +0200

    Factory unit test

---
+++ src/Tests/Factory/dompdfFactoryTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace dompdfmodule\Tests\Factory;
+
+use dompdfmodule\Factory\dompdfFactory;
+
+class dompdfFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new dompdfFactory();
+    }
+
+    public function test_it_it_initializable()
+    {
+        self::assertInstanceOf('dompdfmodule\Factory\dompdfFactory', $this->factory);
+    }
+}
